1e78c706a47f90e7462ef9d1e7fef892e70c4464
CodeWeiName <128158298+CodeWeiName@users.noreply.github.com>
2026-03-21 22:32:51 +0300

    add running job

---
+++ pipeline/run_tests.groovy
@@ -0,0 +1,38 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Run selected tests') {
+            steps {
+                script {
+
+                    def jobs = [:]
+
+                    if (params.RUN_API) {
+                        jobs["API tests"] = {
+                            build job: "api-tests"
+                        }
+                    }
+
+                    if (params.RUN_UI) {
+                        jobs["UI tests"] = {
+                            build job: "ui-test"
+                        }
+                    }
+
+                    if (params.RUN_MOBILE) {
+                        jobs["Mobile tests"] = {
+                            build job: "mobile-tests"
+                        }
+                    }
+
+                    if (jobs.isEmpty()) {
+                        error("No jobs selected")
+                    }
+
+                    parallel jobs
+                }
+            }
+        }
+    }
+}
