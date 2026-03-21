bb786f8bccaa1f7adaadcc0062e5c18bfde66f9a
CodeWeiName <128158298+CodeWeiName@users.noreply.github.com>
2026-03-21 23:11:38 +0300

    add running job add allure

--- pipeline/run_tests.groovy
+++ pipeline/run_tests.groovy
@@ -2,27 +2,29 @@ pipeline {{
     agent any
 
     stages {{
+
         stage('Run selected tests') {{
             steps {{
                 script {{
 
+                    def builds = [:]
                     def jobs = [:]
 
                     if (params.RUN_API) {{
                         jobs["API tests"] = {{
-                            build job: "api-tests"
+                            builds["api"] = build job: "api-tests", wait: true
                         }}
                     }}
 
                     if (params.RUN_UI) {{
                         jobs["UI tests"] = {{
-                            build job: "ui-test"
+                            builds["ui"] = build job: "ui-test", wait: true
                         }}
                     }}
 
                     if (params.RUN_MOBILE) {{
                         jobs["Mobile tests"] = {{
-                            build job: "mobile-tests"
+                            builds["mobile"] = build job: "mobile-tests", wait: true
                         }}
                     }}
 
@@ -31,8 +33,65 @@ pipeline {{
                     }}
 
                     parallel jobs
+
+                    env.API_BUILD = builds["api"]?.number
+                    env.UI_BUILD = builds["ui"]?.number
+                    env.MOBILE_BUILD = builds["mobile"]?.number
                 }}
             }}
         }}
+
+        stage('Collect Allure results') {{
+            steps {{
+                script {{
+
+                    if (params.RUN_API) {{
+                        copyArtifacts(
+                                projectName: "api-tests",
+                                selector: specific(env.API_BUILD),
+                                filter: "target/allure-results/**",
+                                target: "allure/api"
+                        )
+                    }}
+
+                    if (params.RUN_UI) {{
+                        copyArtifacts(
+                                projectName: "ui-test",
+                                selector: specific(env.UI_BUILD),
+                                filter: "target/allure-results/**",
+                                target: "allure/ui"
+                        )
+                    }}
+
+                    if (params.RUN_MOBILE) {{
+                        copyArtifacts(
+                                projectName: "mobile-tests",
+                                selector: specific(env.MOBILE_BUILD),
+                                filter: "target/allure-results/**",
+                                target: "allure/mobile"
+                        )
+                    }}
+
+                }}
+            }}
+        }}
+
+        stage('Merge results') {{
+            steps {{
+                sh '''
+                mkdir -p merged-allure-results
+                find allure -name "*.json" -exec cp {{}} merged-allure-results/ \\;
+                '''
+            }}
+        }}
+
+        stage('Allure report') {{
+            steps {{
+                allure([
+                        results: [[path: 'merged-allure-results']]
+                ])
+            }}
+        }}
+
     }}
 }}
